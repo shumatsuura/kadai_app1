a1ae2dccb9da03d4b58bb108e2550fd5980ed2d5
shumatsuura <shunsuke.mtr@gmail.com>
2020-01-15 13:42:54 +0900

    indexページ実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,23 @@
 class TweetsController < ApplicationController
+  
+  def index
+    @tweets = Tweet.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
 end
