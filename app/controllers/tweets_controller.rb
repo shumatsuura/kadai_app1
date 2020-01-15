86b40382d2976b37f53540d63bbe726cf6f7dcca
shumatsuura <shunsuke.mtr@gmail.com>
2020-01-15 14:00:27 +0900

    削除機能実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only:[:show, :edit,:update,:destroy]
 
   def index
     @tweets = Tweet.all
@@ -28,10 +29,16 @@ class TweetsController < ApplicationController
   end
 
   def destroy
+    @tweet.destroy
+    redirect_to tweets_path, notice:"つぶやきを削除しました！"
   end
 
   private
     def tweet_params
       params.require(:tweet).permit(:content)
+    end
+
+    def set_tweet
+      @tweet = Tweet.find(params[:id])
     end
 end
