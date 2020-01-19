a252843242bf60b3eb96f6ab13e238e447c53ed4
shumatsuura <shunsuke.mtr@gmail.com>
2020-01-19 22:48:12 +0900

    cofig/applicaiton.rbにてgenerate時の設定を変更

--- config/application.rb
+++ config/application.rb
@@ -9,5 +9,10 @@ module KadaiApp1
     config.load_defaults 5.2
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.generators do |g|
+  # この二行の記述で自動生成しない設定を作成しています。
+      g.assets false
+      g.helper false
+    end
   end
 end
